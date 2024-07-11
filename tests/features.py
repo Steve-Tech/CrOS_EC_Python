1bba8c3728e867b70ab0a025c3b2c63248f60678
Stephen Horvath <s.horvath@outlook.com.au>
2024-07-11 10:36:04 +1000

    Restructure API

--- tests/features.py
+++ tests/features.py
@@ -1,19 +1,18 @@
 import unittest
-from cros_ec_python import CrOS_EC
-from cros_ec_python.commands.features import *
+from cros_ec_python import CrOS_EC, features as ec_features
 
 ec = CrOS_EC()
 
 
 class TestGetFeatures(unittest.TestCase):
     def test_version0(self):
-        resp = get_features(ec)
+        resp = ec_features.get_features(ec)
         print(type(self).__name__, "-", "Resp:", resp)
         self.assertIsInstance(resp, int)
 
     def test_decode(self):
-        resp = get_features(ec)
-        decode = decode_features(resp)
+        resp = ec_features.get_features(ec)
+        decode = ec_features.decode_features(resp)
         print(type(self).__name__, "-", "Decode:", decode)
         self.assertIsInstance(decode, list)
 
--- tests/general.py
+++ tests/general.py
@@ -1,14 +1,12 @@
 import unittest
-from cros_ec_python import CrOS_EC
-from cros_ec_python.commands.general import *
-from cros_ec_python.exceptions import ECError
+from cros_ec_python import CrOS_EC, ECError, general as ec_general
 
 ec = CrOS_EC()
 
 
 class TestProtoVersion(unittest.TestCase):
     def test_version0(self):
-        resp = proto_version(ec)
+        resp = ec_general.proto_version(ec)
         print(type(self).__name__, "-", "Resp:", resp)
         self.assertIsInstance(resp, int)
 
@@ -16,20 +14,20 @@ class TestProtoVersion(unittest.TestCase):
 class TestHello(unittest.TestCase):
     def test_version0(self):
         in_data = 42
-        resp = hello(ec, in_data)
+        resp = ec_general.hello(ec, in_data)
         print(type(self).__name__, "-", "Sent:", in_data, "Got:", resp - 0x01020304, "Raw:", resp)
         self.assertEqual(resp, in_data + 0x01020304)
 
 
 class TestGetVersion(unittest.TestCase):
     def test_version0(self):
-        resp = get_version(ec, 0)
+        resp = ec_general.get_version(ec, 0)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, dict)
         self.assertEqual(len(resp), 4)
 
     def test_version1(self):
-        resp = get_version(ec, 1)
+        resp = ec_general.get_version(ec, 1)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, dict)
         self.assertEqual(len(resp), 5)
@@ -37,39 +35,39 @@ class TestGetVersion(unittest.TestCase):
 
 class TestBuildInfo(unittest.TestCase):
     def test_version0(self):
-        resp = get_build_info(ec)
+        resp = ec_general.get_build_info(ec)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, str)
 
 
 class TestChipInfo(unittest.TestCase):
     def test_version0(self):
-        resp = get_chip_info(ec)
+        resp = ec_general.get_chip_info(ec)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, dict)
 
 
 class TestBoardVersion(unittest.TestCase):
     def test_version0(self):
-        resp = get_board_version(ec)
+        resp = ec_general.get_board_version(ec)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, int)
 
 
 class TestCmdVersions(unittest.TestCase):
     def test_versionAny(self):
-        resp = get_cmd_versions(ec, EC_CMD_GET_CMD_VERSIONS)
+        resp = ec_general.get_cmd_versions(ec, ec_general.EC_CMD_GET_CMD_VERSIONS)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, int)
 
     def test_version0(self):
-        resp = get_cmd_versions(ec, EC_CMD_GET_CMD_VERSIONS, 0)
+        resp = ec_general.get_cmd_versions(ec, ec_general.EC_CMD_GET_CMD_VERSIONS, 0)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, int)
         self.assertEqual(resp & 0x1, 0x1)
 
     def test_version1(self):
-        resp = get_cmd_versions(ec, EC_CMD_GET_CMD_VERSIONS, 1)
+        resp = ec_general.get_cmd_versions(ec, ec_general.EC_CMD_GET_CMD_VERSIONS, 1)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, int)
         self.assertEqual(resp & 0x2, 0x2)
@@ -79,26 +77,26 @@ class TestTestProtocol(unittest.TestCase):
     def test_version0(self):
         length = 32
         buf = bytes(range(length))
-        resp = test_protocol(ec, 0, length, buf)
+        resp = ec_general.test_protocol(ec, 0, length, buf)
         print(type(self).__name__, "-", resp)
         self.assertEqual(resp, buf)
 
     def test_error(self):
         with self.assertRaises(ECError):
-            test_protocol(ec, 1, 0, bytes())
+            ec_general.test_protocol(ec, 1, 0, bytes())
 
     def test_warning(self):
         with self.assertWarns(RuntimeWarning):
             length = 16
             buf = bytes(range(length))
-            resp = test_protocol(ec, 0, length, buf, length * 2)
+            resp = ec_general.test_protocol(ec, 0, length, buf, length * 2)
             print(type(self).__name__, "-", resp)
             self.assertEqual(resp[:length], buf)
 
 
 class TestProtocolInfo(unittest.TestCase):
     def test_version0(self):
-        resp = get_protocol_info(ec)
+        resp = ec_general.get_protocol_info(ec)
         print(type(self).__name__, "-", resp)
         self.assertIsInstance(resp, dict)
 
