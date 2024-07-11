f6a41dfe282028c6075fef0f2ae16a807ddb72c8
Stephen Horvath <s.horvath@outlook.com.au>
2024-07-11 12:13:08 +1000

    Add LED command abstractions

---
+++ cros_ec_python/commands/leds.py
@@ -0,0 +1,93 @@
+from typing import Final
+from enum import Enum, auto
+import struct
+from ..cros_ec import CrOS_EC
+from ..constants.COMMON import *
+
+EC_CMD_LED_CONTROL: Final = 0x0029
+
+
+class EcLedId(Enum):
+    # LED to indicate battery state of charge
+    EC_LED_ID_BATTERY_LED = 0
+    #
+    # LED to indicate system power state (on or in suspend).
+    # May be on power button or on C-panel.
+
+    EC_LED_ID_POWER_LED = auto()
+    # LED on power adapter or its plug
+    EC_LED_ID_ADAPTER_LED = auto()
+    # LED to indicate left side
+    EC_LED_ID_LEFT_LED = auto()
+    # LED to indicate right side
+    EC_LED_ID_RIGHT_LED = auto()
+    # LED to indicate recovery mode with HW_REINIT
+    EC_LED_ID_RECOVERY_HW_REINIT_LED = auto()
+    # LED to indicate sysrq debug mode.
+    EC_LED_ID_SYSRQ_DEBUG_LED = auto()
+
+    EC_LED_ID_COUNT = auto()
+
+
+# LED control flags
+EC_LED_FLAGS_QUERY = BIT(0)  # Query LED capability only
+EC_LED_FLAGS_AUTO = BIT(1)  # Switch LED back to automatic control
+
+
+class EcLedColors(Enum):
+    EC_LED_COLOR_RED = 0
+    EC_LED_COLOR_GREEN = auto()
+    EC_LED_COLOR_BLUE = auto()
+    EC_LED_COLOR_YELLOW = auto()
+    EC_LED_COLOR_WHITE = auto()
+    EC_LED_COLOR_AMBER = auto()
+
+    EC_LED_COLOR_COUNT = auto()
+
+
+def led_control(ec: CrOS_EC, led_id: EcLedId, flags: UInt8, brightnesses: list[UInt8]) -> list[UInt8]:
+    """
+    Control an LED
+    @param ec: The CrOS_EC object.
+    @param led_id: The LED to control.
+    @param flags: Control flags. See EC_LED_FLAGS_*.
+    @param brightnesses: Brightness values for each color. (6 colors, see EcLedColors)
+    @return: The available brightness value range for each color.
+    """
+    data = struct.pack(f"<BB{EcLedColors.EC_LED_COLOR_COUNT.value}B", led_id.value, flags, *brightnesses)
+    resp = ec.command(1, EC_CMD_LED_CONTROL, len(data), EcLedColors.EC_LED_COLOR_COUNT.value, data)
+    return list(resp)
+
+
+def led_control_set_color(ec: CrOS_EC, led_id: EcLedId, brightness: UInt8, color: EcLedColors) -> list[UInt8]:
+    """
+    Control an LED
+    @param ec: The CrOS_EC object.
+    @param led_id: The LED to control.
+    @param brightness: Brightness value.
+    @param color: Color to set the LED to.
+    @return: The available brightness value range for each color.
+    """
+    brightnesses = [0] * EcLedColors.EC_LED_COLOR_COUNT.value
+    brightnesses[color.value] = brightness
+    return led_control(ec, led_id, 0, brightnesses)
+
+
+def led_control_get_max_values(ec: CrOS_EC, led_id: EcLedId) -> list[UInt8]:
+    """
+    Get the current brightness values of an LED
+    @param ec: The CrOS_EC object.
+    @param led_id: The LED to get the brightness values of.
+    @return: The available brightness value range for each color.
+    """
+    return led_control(ec, led_id, EC_LED_FLAGS_QUERY, [0] * EcLedColors.EC_LED_COLOR_COUNT.value)
+
+
+def led_control_set_auto(ec: CrOS_EC, led_id: EcLedId) -> list[UInt8]:
+    """
+    Get the current brightness values of an LED
+    @param ec: The CrOS_EC object.
+    @param led_id: The LED to get the brightness values of.
+    @return: The available brightness value range for each color.
+    """
+    return led_control(ec, led_id, EC_LED_FLAGS_AUTO, [0] * EcLedColors.EC_LED_COLOR_COUNT.value)
---
+++ tests/leds.py
@@ -0,0 +1,34 @@
+import unittest
+from cros_ec_python import CrOS_EC, leds as ec_leds
+
+ec = CrOS_EC()
+
+
+class TestLedControl(unittest.TestCase):
+    def test1_led_control_get_max_values(self):
+        resp = ec_leds.led_control_get_max_values(ec, ec_leds.EcLedId.EC_LED_ID_BATTERY_LED)
+        print(type(self).__name__, "-", "Resp:", resp)
+        self.assertIsInstance(resp, list)
+
+    def test2_led_control(self):
+        resp = ec_leds.led_control(ec, ec_leds.EcLedId.EC_LED_ID_BATTERY_LED, 0, [1, 0, 0, 0, 0, 0])
+        print(type(self).__name__, "-", "Resp:", resp)
+        self.assertIsInstance(resp, list)
+        self.assertEqual(input("Is the battery indicator red? [y/N]: "), "y")
+
+    def test3_led_control_set_color(self):
+        resp = ec_leds.led_control_set_color(ec, ec_leds.EcLedId.EC_LED_ID_BATTERY_LED, 1,
+                                             ec_leds.EcLedColors.EC_LED_COLOR_GREEN)
+        print(type(self).__name__, "-", "Resp:", resp)
+        self.assertIsInstance(resp, list)
+        self.assertEqual(input("Is the battery indicator green? [y/N]: "), "y")
+
+    def test4_led_control_set_auto(self):
+        resp = ec_leds.led_control_set_auto(ec, ec_leds.EcLedId.EC_LED_ID_BATTERY_LED)
+        print(type(self).__name__, "-", "Resp:", resp)
+        self.assertIsInstance(resp, list)
+        self.assertEqual(input("Is the battery indicator normal? [y/N]: "), "y")
+
+
+if __name__ == '__main__':
+    unittest.main()
